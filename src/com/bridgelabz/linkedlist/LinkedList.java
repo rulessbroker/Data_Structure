f4a159fc52e730016d089c505f76bc612b4e71cc
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-11-23 16:34:48 +0530

    Added: Simple linked list

---
+++ src/com/bridgelabz/linkedlist/LinkedList.java
@@ -0,0 +1,32 @@
+package com.bridgelabz.linkedlist;
+
+public class LinkedList {
+	Node head;
+
+	public void insert(int data) {
+		Node newNode = new Node();
+		newNode.data = data;
+		newNode.next = null;
+
+		if (head == null) {
+			head = newNode;
+		} else {
+			Node temp = head;
+			while (temp.next != null) {
+				temp = temp.next;
+			}
+			temp.next = newNode;
+		}
+	}
+
+	public void show() {
+		Node node = head;
+
+		while (node.next != null) {
+			System.out.println(node.data);
+			node = node.next;
+		}
+		System.out.println(node.data);
+	}
+
+}
---
+++ src/com/bridgelabz/linkedlist/SingleLinkedList.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.linkedlist;
+
+public class SingleLinkedList {
+
+	public static void main(String[] args) {
+		LinkedList list = new LinkedList();
+
+		list.insert(56);
+		list.insert(30);
+		list.insert(70);
+
+		list.show();
+	}
+}
